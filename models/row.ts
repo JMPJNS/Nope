8e68758ad162f928d1f09f057e2798b799b7fff9
Jonas <jonas@jmp.blue>
2022-01-13 09:37:49 +0100

    init

---
+++ models/row.ts
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export class Row {
+    id: string
+    colls: Column<ColumnType>[]
+    created: Date
+
+    constructor() {
+        this.id = uuidv4()
+        this.colls = []
+        this.created = new Date()
+    }
+}
+
+export class Column<T extends ColumnType> {
+    id: string
+    type: T
+    data?: ColumnDataType<T>
+    created: Date
+
+    constructor(type: T, data?: ColumnDataType<T>) {
+        this.id = uuidv4()
+        this.type = type
+        this.created = new Date()
+        this.data = data
+    }
+}
+
+export enum ColumnType {
+    Number,
+    String,
+    Boolean,
+    Date,
+    Timespan,
+    Image,
+    ColumnReference,
+    RowReference,
+    Note
+}
+
+type ColumnDataType <K extends ColumnType> =
+    K extends ColumnType.Number ? number :
+    K extends ColumnType.String ? string :
+    K extends ColumnType.Boolean ? boolean :
+    K extends ColumnType.Date ? Date :
+    K extends ColumnType.Timespan ? {start: Date, end: Date} :
+    K extends ColumnType.Image ? string : // URL To Image
+    K extends ColumnType.ColumnReference ? string : // id to other Column
+    K extends ColumnType.RowReference ? string : // id to other Row
+    K extends ColumnType.Note ? string : any // markdown stored as string
---
+++ services/rowService.ts
@@ -0,0 +1,39 @@
+import { Column, ColumnType, Row } from "../models/row"
+import { v4 as uuidv4 } from 'uuid'
+
+class RowService {
+    rowDb: Row[] = [
+        {
+            id: uuidv4(),
+            created: new Date(),
+            colls: [
+                new Column(ColumnType.Number, 2),
+                new Column(ColumnType.String, "Hello"),
+                new Column(ColumnType.Date, new Date())
+            ]
+        }
+    ]
+
+    async addRow(r: Row) {
+        if (this.rowDb.find(x => x.id === r.id)) return
+        this.rowDb.push(r)
+    }
+
+    async getRow(id: string) {
+        return this.rowDb.find(x => x.id === id)
+    }
+
+    async deleteRow(id: string) {
+        this.rowDb = this.rowDb.filter(x => x.id !== id)
+    }
+
+    async getRows(skip: number = 0, take: number = this.rowDb.length) {
+        return this.rowDb
+    }
+
+    async init() {
+        return this
+    }
+}
+
+export default await new RowService().init()
